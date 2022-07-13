20ec55c58c361cfa61740676b4b0fc0bb9611876
Grey <lserg3@yandex.ru>
2022-07-13 12:17:23 +0300

    сделал задачу 2.9

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppMoviesProps = {
+  name: string;
+  genre: string;
+  release: number;
+};
+
+function App({ name, genre, release }: AppMoviesProps): JSX.Element {
+  return <MainScreen filmName={name} filmGenre={genre} filmRelease={release} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Film = {
+  name: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  release: 2014,
+};
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App
+      name={Film.name}
+      genre={Film.genre}
+      release={Film.release}
+    />
+  </React.StrictMode>
 );
